f2c98ece1e631786fde092a364e3eb14fe637904
joaopedropb2005 <jppb2005normal@gmail.com>
2026-02-10 12:51:07 -0300

    config pom.xml para testes

---
+++ src/test/java/com/mycompany/findem/service/AnimalServiceTest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.findem.service;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+
+
+/**
+ *
+ * @author jppb2
+ */
+public class AnimalServiceTest {
+    
+    @Test
+    void testeDeConfiguracao(){
+        assertTrue(true);
+    }
+    
+}
